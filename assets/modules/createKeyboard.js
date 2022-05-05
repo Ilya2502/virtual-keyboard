227d3b5f99940897936640e0eabbe9ae79b61585
Ilya2502 <gavran2502@mail.ru>
2022-05-06 01:02:51 +0300

    feat: add css, 2 modules, change keysBase (add key and code), add listeners click, keydown, keyup

---
+++ assets/modules/createKeyboard.js
@@ -0,0 +1,34 @@
+import {keysArr} from './keysArr.js';
+
+
+export function createKeyboard() {
+    let divTitle = document.createElement('h1');
+    divTitle.className = "title";
+    divTitle.id = "title";
+    divTitle.innerHTML = "RSS Виртуальная клавиатура";
+    document.body.append(divTitle);
+
+    let textArea = document.createElement('textarea');
+    textArea.className = "textarea";
+    textArea.rows = "8";
+    textArea.cols = "50";
+    document.body.append(textArea);
+
+    let keyboardContainer = document.createElement('div');
+    keyboardContainer.className = "keyboard-container";
+    document.body.append(keyboardContainer);
+    for (let i = 0; i < keysArr.length; i++) {
+        let keyboardRow = document.createElement('div');
+        keyboardRow.className = "keyboard-row";
+        keyboardContainer.append(keyboardRow);
+        for (let j = 0; j < keysArr[i].length; j++) {
+            let key = document.createElement('div');
+            key.className = `key keyInner ${keysArr[i][j].code}`;
+            key.id = keysArr[i][j].code;
+            key.innerHTML = keysArr[i][j].key;
+            keyboardRow.append(key);
+        }
+    }
+    const keyNotInner = document.querySelectorAll('.Backspace, .Tab, .Delete, .CapsLock, .Enter, .ShiftLeft, .ShiftRight, .ArrowUp, .ControlLeft, .MetaLeft, .AltLeft, .AltRight, .ArrowLeft, .ArrowDown, .ArrowRight, .ControlRight');
+    keyNotInner.forEach(item => item.classList.remove('keyInner'));
+  }
--- index.js
+++ index.js
@@ -1,36 +1,78 @@
-let divTitle = document.createElement('h1');
-divTitle.className = "title";
-divTitle.id = "title";
-divTitle.innerHTML = "RSS Виртуальная клавиатура";
-document.body.append(divTitle);
-
-let textArea = document.createElement('textarea');
-textArea.className = "textarea";
-textArea.rows = "14";
-textArea.cols = "100";
-document.body.append(textArea);
-
-function createKeyboard() {
-    let keyboardContainer = document.createElement('div');
-    keyboardContainer.className = "keyboard-container";
-    document.body.append(keyboardContainer);
-    let keysArr = [["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-                   ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del"],
-                   ["CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "\'", "Enter"],
-                   ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&#9650"],
-                   ["Ctrl", "Win", "Alt", "space", "Alt", "&#9664;", "&#9660", "&#9654;", "Ctrl"]];
-    for (let i = 0; i < keysArr.length; i++) {
-        let keyboardRow = document.createElement('div');
-        keyboardRow.className = "keyboard-row";
-        keyboardContainer.append(keyboardRow);
-        for (let j = 0; j < keysArr[i].length; j++) {
-            let key = document.createElement('div');
-            key.className = `key ${keysArr[i][j]}`;
-            key.id = keysArr[i][j];
-            key.innerHTML = keysArr[i][j];
-            keyboardRow.append(key);
-        }
-    }
-}
+import {keysArr} from './assets/modules/keysArr.js';
+import {createKeyboard} from './assets/modules/createKeyboard.js';
 
 createKeyboard()
+
+const textArea = document.querySelector('.textarea');
+let textaeraCursorStart = 0;
+let textaeraCursorEnd = 0;
+const allKeysInner = document.querySelectorAll('.keyInner');
+const backspaceKey = document.querySelector('.Backspace');
+
+
+function addClickKeyInner(event) {
+    textArea.focus();
+    textArea.innerHTML = textArea.innerHTML + event.target.innerHTML;
+    textaeraCursorStart += 1;
+    textaeraCursorEnd += 1;
+    textArea.setSelectionRange(textaeraCursorStart, textaeraCursorEnd);
+    // textArea.selectionStart -= 1;
+    // textArea.selectionEnd -= 1;
+}
+
+function backspace() {
+    textArea.focus();
+    textArea.innerHTML = textArea.innerHTML.slice(0, -1);
+    if (textaeraCursorStart > 0 && textaeraCursorEnd > 0) {
+        textaeraCursorStart -= 1;
+        textaeraCursorEnd -= 1;
+    }
+    textArea.setSelectionRange(textaeraCursorStart, textaeraCursorEnd);
+}
+
+function addKeyDownInner(event) {
+    let activeKey = document.querySelector(`#${event.code}`);
+    activeKey.classList.add('active');
+    if (activeKey.classList.contains('keyInner')) {
+        textArea.focus();
+        textArea.innerHTML = textArea.innerHTML + activeKey.innerHTML;
+        textaeraCursorStart += 1;
+        textaeraCursorEnd += 1;
+        textArea.setSelectionRange(textaeraCursorStart, textaeraCursorEnd);
+    }
+    
+    // if (event.code.slice(0, -1) === 'Digit' || event.code.slice(0, -1) === 'Key') {
+    //     let activeKey = document.querySelector('#key'+ event.code.slice(-1).toLowerCase());
+    //     activeKey.classList.add('active');
+    //     textArea.focus();
+    //     textArea.innerHTML = textArea.innerHTML + activeKey.innerHTML;
+    //     textaeraCursorStart += 1;
+    //     textaeraCursorEnd += 1;
+    //     textArea.setSelectionRange(textaeraCursorStart, textaeraCursorEnd);
+    // } else {
+    //     let activeKey = document.querySelector('.'+ event.code);
+    //     activeKey.classList.add('active');
+
+
+
+
+        // textArea.focus();
+        // textArea.innerHTML = textArea.innerHTML + activeKey.innerHTML;
+        // textaeraCursorStart += 1;
+        // textaeraCursorEnd += 1;
+        // textArea.setSelectionRange(textaeraCursorStart, textaeraCursorEnd);
+   // }
+}
+
+function addKeyUp(event) {
+    let activeKey = document.querySelector(`#${event.code}`);
+    activeKey.classList.remove('active');
+}
+
+
+backspaceKey.addEventListener('click', backspace)
+allKeysInner.forEach((item) => {
+    item.addEventListener('click', addClickKeyInner)
+})
+document.addEventListener('keydown', addKeyDownInner);
+document.addEventListener('keyup', addKeyUp);
